fcb37f20fa3050c567038fdd4006fb29d29f6996
takayama shunya <katonbo1@icloud.com>
2020-06-13 22:02:32 +0900

    6/13 授業

---
+++ main.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('#datepicker').datepicker();
+});
+
+$('#name').click(function(){
+  $('.color_name').css("color","red");
+});
+
+$('#adress').click(function(){
+  $('.color_adress').css("color","red");
+});
+
+$('.sex').click(function(){
+  $('.color_sex').css("color","red");
+});
+
+$('#datepicker').click(function(){
+  $('.color_datepicker').css("color","red");
+});
+
+$('#form').click(function(){
+  $('.color_form').css("color","red");
+});
